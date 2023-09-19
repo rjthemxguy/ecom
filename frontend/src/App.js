9c32dd80d2fdc97347e4562cec5e1d3a6036b44b
root <rjrseo@gmail.com>
2023-09-19 04:06:53 +0000

    rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
+import { Outlet  } from "react-router-dom"
+
 
 const App = () => {
   return (
-    <h1>Welcome</h1>
-      
+    <>
+     <Header/>
+      <main className="py-3">
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer/>
+    </>  
     
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import {Navbar, Nav, Container} from 'react-bootstrap'
+import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import logo from '../assets/logo.png'
+import {LinkContainer} from 'react-router-bootstrap'
+
+
+const Header = () => {
+  return (
+   <header>
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+
+    <Container>
+
+      <LinkContainer to="/">
+      <Navbar.Brand>
+        <img src={logo}/>
+        Proshop
+      </Navbar.Brand>
+      </LinkContainer>
+
+      <Navbar.Toggle aria-controls = "basic-navbar-nav"/>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className='ms-auto'>
+
+          <LinkContainer to="/cart">
+            <Nav.Link href="/cart"><FaShoppingCart/>Cart</Nav.Link>
+          </LinkContainer>
+
+          <LinkContainer to="login">
+            <Nav.Link href="login"><FaUser/>Login</Nav.Link>
+          </LinkContainer>
+        </Nav>
+
+      </Navbar.Collapse>
+
+    </Container>
+    </Navbar>
+   </header>
+  )
+}
+
+export default Header
